39c781ba178cd8e78b948e489d1c4731cd935d20
Brian Waithaka <waithakabrian8@gmail.com>
2024-07-26 20:19:36 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
 export default App;
+// Mortgage Calculator
+// Clear All
+
+// Mortgage Amount
+
+// Mortgage Term
+
+// Interest Rate
+
+// Mortgage Type
+// Repayment
+// Interest Only
+
+// Calculate Repayments
+
+// <!-- Empty results start -->
+
+// Results shown here
+
+// Complete the form and click “calculate repayments” to see what 
+// your monthly repayments would be.
+
+// <!-- Empty results end -->
+
+// <!-- Completed results start -->
+
+// Your results
+
+// Your results are shown below based on the information you provided. 
+// To adjust the results, edit the form and click “calculate repayments” again.
+
+// Your monthly repayments
+
+// Total you'll repay over the term
