c0f140e9c7f168ef473ae03b1644fa3fad704193
Brian Waithaka <waithakabrian8@gmail.com>
2024-07-27 21:15:30 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,95 @@
-
-import './App.css';
+import "./App.css";
+import imageCalculator from "./images/icon-calculator.svg";
+import imagePlaceholder from "./images/illustration-empty.svg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Form />
+      <DisplayResults />
+    </div>
+  );
+}
 
+function Form() {
+  return (
+    <form>
+      <div className="flex-row margin-bottom">
+        <h2 style={{ color: "hsl(202, 55%, 16%)" }}>Mortgage Calculator</h2>
+        <span style={{ color: "hsl(200, 24%, 40%)" }}>Clear All</span>
+      </div>
+      <Input id={"MortgageAmount"} classy={"flex-col"}>
+        Mortgage Amount
+      </Input>
+      <div className="flex-row margin-top">
+        <Input id={"MortgageTerm"} classy={"flex-col"}>
+          Mortgage Term
+        </Input>
+        <Input id={"InterestRate"} classy={"flex-col"}>
+          Interest Rate
+        </Input>
+      </div>
+      <div className="flex-col margin-top">
+        <label style={{ color: "hsl(200, 24%, 40%)" }}>Mortgage Type</label>
+        <RadioInput id={"Repayment"}>Repayment</RadioInput>
+        <RadioInput id={"Interest-Only"}>Interest Only</RadioInput>
+      </div>
+      <Button>
+        <img src={imageCalculator} alt="calculator" />{" "}
+        <span style={{ color: "hsl(202, 55%, 16%)" }}>
+          Calculate Repayments
+        </span>
+      </Button>
+    </form>
+  );
+}
+
+function DisplayResults() {
+  return (
+    <>
+      <Placeholder />
+    </>
+  );
+}
+
+function Placeholder() {
+  return (
+    <div className="background">
+    <img src={imagePlaceholder} alt="calculator"/>
+      <h2 style={{color:"hsl(202, 86%, 94%)"}}>Results shown here</h2>
+      <p style={{color:" hsl(200, 26%, 54%)"}}>
+        Complete the form and click “calculate repayments” to see what your
+        monthly repayments would be.
+      </p>
+    </div>
+  );
+}
+
+function Button({ children }) {
+  return <button className="btn flex margin-top">{children}</button>;
+}
+
+function RadioInput({ children, id }) {
+  return (
+    <div className="radio">
+      <input type="radio" name="mortgage-type" id={id} />
+      <label
+        htmlFor={id}
+        style={{ color: "hsl(202, 55%, 16%)", marginLeft: "0.5em" }}
+      >
+        {children}
+      </label>
+    </div>
+  );
+}
+
+function Input({ children, id, classy }) {
+  return (
+    <div className={classy}>
+      <label htmlFor={id} style={{ color: "hsl(200, 24%, 40%)" }}>
+        {children}
+      </label>
+      <input type="text" id={id} />
     </div>
   );
 }
@@ -29,7 +114,7 @@ export default App;
 
 // Results shown here
 
-// Complete the form and click “calculate repayments” to see what 
+// Complete the form and click “calculate repayments” to see what
 // your monthly repayments would be.
 
 // <!-- Empty results end -->
@@ -38,7 +123,7 @@ export default App;
 
 // Your results
 
-// Your results are shown below based on the information you provided. 
+// Your results are shown below based on the information you provided.
 // To adjust the results, edit the form and click “calculate repayments” again.
 
 // Your monthly repayments
